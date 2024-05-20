1de354b2bb198222022f9b38b602f8358440ef83
danfacerea <169090372+danfacerea@users.noreply.github.com>
2024-05-20 22:09:37 +0300

    Update page.tsx

--- src/app/[locale]/read-book/page.tsx
+++ src/app/[locale]/read-book/page.tsx
@@ -10,7 +10,7 @@ const ReadBookPage = () => {
 	const hasWindow = typeof window !== "undefined"
 	const offset = 7
 	const maxPage = currentLocale === "en" ? 359 : 350
-	const collapseWidth = 1280
+	const collapseWidth = 1400 //1280
 	const bookViewer = useRef<HTMLDivElement>(null)
 
 	const checkOnePage = useCallback(() => {
@@ -88,7 +88,7 @@ const ReadBookPage = () => {
 			>
 				<div id="lpage" className={`${fullscreen ? "" : "flex-1"}`}>
 					<img
-						width={900}
+						width={720}
 						height={1024}
 						src={`https://facerea.ro/img${currentLocale}/${page}.jpg`}
 						alt={`${t("page")} ${page - offset}`}
@@ -103,7 +103,7 @@ const ReadBookPage = () => {
 					}`}
 				>
 					<img
-						width={900}
+						width={720}
 						height={1440}
 						src={`https://facerea.ro/img${currentLocale}/${page + 1}.jpg`}
 						alt={`${t("page")} ${page - offset}`}
