0948f512ad4c675be397137d528c93d267849dc0
danfacerea <169090372+danfacerea@users.noreply.github.com>
2024-05-21 16:05:03 +0300

    Update page.tsx

--- src/app/[locale]/order/page.tsx
+++ src/app/[locale]/order/page.tsx
@@ -4,21 +4,6 @@ import { useScopedI18n } from "@/locales/client"
 import { redirect } from "next/navigation"
 import { useEffect, useState } from "react"
 
-//import React from "react"
-interface PopupProps {
-  message: string;
-  onClose: () => void
-}
-
-const Popup: React.FC<PopupProps> = ({ message, onClose }) => (
-  <div className="popup">
-    <p>{message}</p>
-    <button onClick={onClose}>OK</button>
-  </div>
-)
-
-export default Popup
-
 
 const Order = () => {
 	const t = useScopedI18n("Order")
@@ -37,4 +22,4 @@ const Order = () => {
 		</div>
 	)
 }
-export default Order
+//export default Order
