df95e5d30e396fef443f516254520246f86fa7d8
danfacerea <169090372+danfacerea@users.noreply.github.com>
2024-05-21 12:02:37 +0300

    Update Posts.ts

--- payload/collections/Posts.ts
+++ payload/collections/Posts.ts
@@ -72,7 +72,14 @@ const Posts: CollectionConfig = {
 				readOnly: true,
 			},
 			hooks: {
-				beforeChange: [async (args: FieldHookArgs<Post, string, Post>) => {
+
+				
+  beforeChange: [({ value }: FieldHookArgs<Post, string, Post>) => value ?? new Date().toISOString()],
+  afterRead: [({ value }: FieldHookArgs<Post, string, Post>) => new Date(value ?? new Date().getTime()).toLocaleDateString("en-US", { day: "2-digit", month: "2-digit", year: "4-digit" })],
+},
+
+				
+				//beforeChange: [async (args: FieldHookArgs<Post, string, Post>) => {
 					if (args.value) return args.value
 					const slug = slugify(args.siblingData.titleRomanian ?? "")
 					let uuid = v4().slice(0, 8)
