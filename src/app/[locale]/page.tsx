86fb99754491097da5d78a95c4f2057f31533caf
danfacerea <169090372+danfacerea@users.noreply.github.com>
2024-07-08 19:50:12 +0300

    Update page.tsx
    Mutare buton read book

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -15,7 +15,8 @@ const Page = async () => {
           alt="Banner"
           className="w-full h-auto"
           src={currentLocale === "en" ? bannerEn.src : bannerRo.src}
-        />
+		  />
+		  
       </div>
       <div className="w-full px-8 md:px-4 py-6 bg-gray-300 border-y-2 border-gray-400 flex flex-col md:flex-row place-content-evenly gap-4">
         <div className="flex justify-center">
@@ -24,16 +25,17 @@ const Page = async () => {
             className="h-full max-h-96 w-auto"
             src={`https://facerea.ro/img${currentLocale}/Coperta.jpg`}
           />
+		  
         </div>
-        <div className="flex flex-col gap-4 justify-center items-center">
+		<div className="flex flex-col gap-4 justify-center items-center">
           <span className="text-4xl font-bold">{t("title")}</span>
           <span className="text-2xl font-medium">{t("subtitle")}</span>
           <Link
             href="/read-book"
-            className="text-2xl rounded-2xl p-5 border-2 border-slate-400 bg-slate-300 hover:bg-slate-200"
+            className="text-2xl rounded-2xl p-5 border-2 border-slate-500 bg-slate-300 hover:bg-slate-200"
           >
-            {t("read")}
-          </Link>
+          {t("read")}
+        </Link>
         </div>
       </div>
     </div>
