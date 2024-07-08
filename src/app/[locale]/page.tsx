1930b956dfc93bbecfe1368cbeeb836b007876f2
danfacerea <169090372+danfacerea@users.noreply.github.com>
2024-07-08 21:49:26 +0300

    Update page.tsx

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -15,8 +15,7 @@ const Page = async () => {
           alt="Banner"
           className="w-full h-auto"
           src={currentLocale === "en" ? bannerEn.src : bannerRo.src}
-		  />
-		  
+        />
       </div>
       <div className="w-full px-8 md:px-4 py-6 bg-gray-300 border-y-2 border-gray-400 flex flex-col md:flex-row place-content-evenly gap-4">
         <div className="flex justify-center">
@@ -25,17 +24,18 @@ const Page = async () => {
             className="h-full max-h-96 w-auto"
             src={`https://facerea.ro/img${currentLocale}/Coperta.jpg`}
           />
-		  
         </div>
-	<div className="flex flex-col gap-4 justify-center items-center">
-          <span className="text-4xl font-bold">{t("title")}</span>
-          <span className="text-2xl font-medium">{t("subtitle")}</span>
-          <Link
-            href="/read-book"
-            className="text-2xl rounded-2xl p-5 border-2 border-slate-500 bg-slate-300 hover:bg-slate-200"
-          >
-          {t("read")}
-        </Link>
+        <div className="flex flex-col gap-4 justify-center items-center md:items-start">
+          <span className="text-4xl font-bold text-center md:text-left">{t("title")}</span>
+          <span className="text-2xl font-medium text-center md:text-left">{t("subtitle")}</span>
+          <div className="mt-4 flex justify-center md:justify-start w-full">
+            <Link
+              href="/read-book"
+              className="text-2xl rounded-2xl p-5 border-2 border-slate-400 bg-slate-300 hover:bg-slate-200"
+            >
+              {t("read")}
+            </Link>
+          </div>
         </div>
       </div>
     </div>
