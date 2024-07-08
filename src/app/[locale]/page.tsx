ce453ec27a65ef2df4e8983382466d1dfc10668c
danfacerea <169090372+danfacerea@users.noreply.github.com>
2024-07-08 19:33:35 +0300

    Update page.tsx

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
+/* eslint-disable @next/next/no-img-element */
 import bannerEn from "@/assets/banner_en.jpg";
 import bannerRo from "@/assets/banner_ro.jpg";
 import { getCurrentLocale, getScopedI18n } from "@/locales/server";
@@ -15,7 +16,8 @@ const Page = async () => {
           alt="Banner"
           className="w-full h-auto"
           src={currentLocale === "en" ? bannerEn.src : bannerRo.src}
-        />
+		  />
+		  
       </div>
       <div className="w-full px-8 md:px-4 py-6 bg-gray-300 border-y-2 border-gray-400 flex flex-col md:flex-row place-content-evenly gap-4">
         <div className="flex justify-center">
@@ -24,19 +26,20 @@ const Page = async () => {
             className="h-full max-h-96 w-auto"
             src={`https://facerea.ro/img${currentLocale}/Coperta.jpg`}
           />
+		  
         </div>
-        <div className="flex flex-col gap-4 justify-center items-center md:items-start md:justify-center">
-          <span className="text-4xl font-bold text-center md:text-left">{t("title")}</span>
-          <span className="text-2xl font-medium text-center md:text-left">{t("subtitle")}</span>
-          <div style={{ marginTop: '1rem', textAlign: 'center', width: '100%' }}>
-            <Link
-              href="/read-book"
-              className="text-2xl rounded-2xl p-5 border-2 border-slate-400 bg-slate-300 hover:bg-slate-200"
-              style={{ display: 'inline-block' }}
-            >
-              {t("read")}
-            </Link>
-          </div>
+		<div className="flex flex-col gap-4 justify-top items-center">
+		<Link
+            href="/read-book"
+            className="text-2xl rounded-2xl p-5 border-2 border-slate-500 bg-slate-300 hover:bg-slate-200"
+          >
+            {t("read")}
+        </Link>
+		</div>
+		<div className="flex flex-col gap-4 justify-center items-center">
+          <span className="text-4xl font-bold">{t("title")}</span>
+          <span className="text-2xl font-medium">{t("subtitle")}</span>
+          
         </div>
       </div>
     </div>
