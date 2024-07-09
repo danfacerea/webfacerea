026319b85b2ee98129eebf0976493b05a3ba951d
danfacerea <169090372+danfacerea@users.noreply.github.com>
2024-07-09 10:03:32 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,20 +1,24 @@
-const { withPayload } = require("@payloadcms/next/withPayload");
-const path = require("path");
+onst { withPayload } = require("@payloadcms/next/withPayload")
+const path = require("path")
 
 /** @type {import("next").NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  reactStrictMode: true, // Set to true for stricter React mode
-  images: {
-    domains: ["localhost", "facerea.ro"], // Add domains for Image component
-  },
-  webpack(config) {
-    return config;
-  },
-};
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
+	reactStrictMode: false,
+	images: {
+		remotePatterns: [
+			{
+				protocol: "http",
+				hostname: "localhost",
+			},
+			{
+				protocol: "https",
+				hostname: "facerea.ro",
+			},
+		],
+	}
+}
 
-module.exports = withPayload(nextConfig, {
-  configPath: path.resolve(__dirname, "payload", "payload.config.ts"),
-});
+module.exports = withPayload(nextConfig, { configPath: path.resolve(__dirname, "payload", "payload.config.ts") })
