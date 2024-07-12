fa8ed63ed420874c5a97721e6c3125eeb5e21742
danfacerea <169090372+danfacerea@users.noreply.github.com>
2024-07-12 22:26:33 +0300

    index.js

---
+++ src/app/[locale]/index.js
@@ -0,0 +1,16 @@
+// pages/index.js (Next.js example)
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Home Page</title>
+        <meta name="description" content="Facerea de la Geneza la Apocalipsa." />
+        <meta name="keywords" content="home, next.js" />
+        <!-- Add other meta tags here -->
+      </Head>
+      <h1>Welcome to the Home Page</h1>
+    </div>
+  );
+}
