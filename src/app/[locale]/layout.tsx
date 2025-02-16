2bbe878cd2d5ab6a643b4cce86b4d32c6b4d5f1f
danfacerea <169090372+danfacerea@users.noreply.github.com>
2025-02-16 16:49:03 +0200

    Update layout.tsx
    Added HTML tag from google

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -42,11 +42,17 @@ const Layout = ({ children, params }: { children: React.ReactNode; params: { loc
 
   const currentMetadata = metadata[params.locale] || metadata.en;
 
+
+<meta name="google-site-verification" content="4iJ3tJK7GGS8BmUwUhlUOIuA8yzUn-D7IytPw22c4TI" />
+
+
+
   return (
     <html lang={params.locale}>
       <Head>
         <title>{currentMetadata.title}</title>
-        <meta name="description" content={currentMetadata.description} />
+        //<meta name="description" content={currentMetadata.description} />
+		<meta name="google-site-verification" content="4iJ3tJK7GGS8BmUwUhlUOIuA8yzUn-D7IytPw22c4TI" />
         <meta property="og:title" content={currentMetadata.ogTitle} />
         <meta property="og:description" content={currentMetadata.ogDescription} />
         <meta property="og:type" content="website" />
