185baf77d97bd4473094a200688a1b5d27a7ee3e
danfacerea <169090372+danfacerea@users.noreply.github.com>
2025-07-21 20:46:23 +0300

    Mutat toate svg in public

--- src/app/_components/LanguagePicker.tsx
+++ src/app/_components/LanguagePicker.tsx
@@ -1,18 +1,16 @@
 /* eslint-disable @next/next/no-img-element */
 "use client"
 
-import flagEn from "@/assets/en.svg"
-import flagRo from "@/assets/ro.svg"
 import { useChangeLocale, useCurrentLocale, useScopedI18n } from "@/locales/client"
 import { Menu } from "@headlessui/react"
 
 const langs = {
 	en: {
-		flag: flagEn,
+		flag: "/en.svg",
 		key: "english",
 	},
 	ro: {
-		flag: flagRo,
+		flag: "/ro.svg",
 		key: "romanian",
 	},
 } as const
@@ -34,7 +32,7 @@ export const LanguagePicker = (props: LanguagePickerProps) => {
 					as="button"
 					className="flex gap-2 items-center rounded-md p-2 w-full text-gray-400 bg-gray-700/50 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
 				>
-					<img alt={langs[currentLocale].key} src={langs[currentLocale].flag.src} width="20" height="20" />
+					<img alt={langs[currentLocale].key} src={langs[currentLocale].flag} width="20" height="20" />
 					{t("language")}
 				</Menu.Button>
 				<Menu.Items className="origin-top-right absolute right-0 mt-2 w-max rounded-md shadow-lg bg-gray-900 flex flex-col gap-2 p-2">
@@ -45,7 +43,7 @@ export const LanguagePicker = (props: LanguagePickerProps) => {
 							key={lang.key}
 							className="flex-1 flex gap-2 items-center rounded-md px-2 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
 						>
-							<img alt={lang.key} src={lang.flag.src} width="20" height="20" />
+							<img alt={lang.key} src={lang.flag} width="20" height="20" />
 							{t(lang.key)}
 						</Menu.Item>
 					))}
@@ -60,7 +58,7 @@ export const LanguagePicker = (props: LanguagePickerProps) => {
 						currentLocale === code ? "bg-gray-700" : ""
 					}`}
 					>
-						<img alt={lang.key} src={lang.flag.src} width="20" height="20" />
+						<img alt={lang.key} src={lang.flag} width="20" height="20" />
 						{t(lang.key)}
 					</button>
 				))}
